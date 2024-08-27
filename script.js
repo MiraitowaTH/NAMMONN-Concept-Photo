10983cf83212d6f1411bec28d4f800bdc83341da
MiraitowaTH <saonoibnk48fanclub@gmail.com>
2024-08-27 13:55:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tweetMessage() {
-    const message = "MIRAI TO WA KHAIMOOK\n#MiraitowaTH #KhaimookBNK48 ";
+    const message = "MIRAI TO WA NAMMONN\n#MiraitowaTH #NammonnBNK48 ";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 } 
